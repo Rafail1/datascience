dd0ed40457941843e8cd5a829b9ca209e2e1c995
Rafail1 <ya.rafon-92@ya.ru>
2019-03-29 16:38:05 +0300

    а

---
+++ venv/functions.py
@@ -0,0 +1,100 @@
+import math
+
+
+def vector_add(v, w):
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+def vector_subtract(v, w):
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+
+def vector_sum(vectors):
+    result = vectors[0]
+    for vector in vectors[1:]:
+        result = vector_add(result, vector)
+    return result
+
+
+def scalar_multiply(c, v):
+    return [c * v_i for v_i in v]
+
+
+def vector_mean(vectors):
+    n = len(vectors)
+    return scalar_multiply(1 / n, vector_sum(vectors))
+
+
+def dot(v, w):
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+def sum_of_squares(v):
+    return dot(v, v)
+
+
+def magnitude(v):
+    return math.sqrt(sum_of_squares(v))
+
+
+def squared_distance(v, w):
+    return sum_of_squares(vector_subtract(v, w))
+
+
+def distance(v, w):
+    return magnitude(vector_subtract(v, w))
+
+
+def mean(x):
+    return sum(x) / len(x)
+
+
+def de_mean(x):
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+
+def variance(x):
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations) / (n - 1)
+
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+
+def quantile(x, p):
+    p_index = int(p * len(x))
+    return sorted(x)[p_index]
+
+
+def interquartile_range(x):
+    return quantile(x, 0.75) - quantile(x, 0.25)
+
+
+def covariance(x, y):
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n-1)
+
+
+def correlation(x, y):
+    stdev_x = standard_deviation(x)
+    stdev_y = standard_deviation(y)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(x, y) / stdev_x / stdev_y
+    else:
+        return 0
+
+
+num_friends = [0, 2, 8, 11, 14]
+
+daily_minutes = [1, 2, 4, 7, 10]
+
+print(standard_deviation(num_friends))
+
+print(standard_deviation(daily_minutes))
+
+print(dot(de_mean(num_friends), de_mean(daily_minutes)) / 5)
+
+print(correlation(num_friends, daily_minutes))
